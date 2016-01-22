43d7fd1e9ffd70b0f7cb930744e562cc76f86d6c
Jordan Roper <jordanroper@gmail.com>
2016-01-21 21:12:07 -0700

    finished project

--- practice.js
+++ practice.js
@@ -5,29 +5,33 @@
 
   //Code here
 
+var me = {name: "Jordan", age:"27"};
+//alert (me.name);
 
 
 
 //NEXT PROBLEM
 
-
-
-
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
   //Code here
+
+  var favoriteThings = {band: "Iron Maiden", food: "Italian", person: "Clint", book: "Food", movie: "2 Fast 2 Furious", holiday: "Christmas"};
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Code here
 
+favoriteThings.car = "Mercedes";
+favoriteThings.brand = "Apple";
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
   //Code here
 
-
+favoriteThings.food = "Lettuce";// {food: "Lettuce", book: "50 Shades of Gray"};
+favoriteThings.book = "50 Shades of Gray";
 
 
 //NEXT PROBLEM
@@ -44,9 +48,19 @@ that is named color, with the value being the color of your backpack. */
 
   //Code here
 
+  var backPack = {};
+
+  var item = "firstPocket";
+
+  backPack.firstPocket = "chapstick";
+
+  item.color = "grey";
+
 //After you do the above, alert your entire backPack object.
 
   //Code here
+
+      // alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -54,7 +68,7 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code here
 
-
+console.log(backPack);
 
 
 //NEXT PROBLEM
@@ -66,11 +80,23 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code Here
 
+  var alsoMe = {
+    name: "Jordan",
+    age: "27",
+    height: "6'2",
+    gender: "Male",
+    married: "Yes",
+    eyeColor: "Blue",
+    hairColor: "Blonde",
+  }
+
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
   //Code Here
 
-
+//for (var key in alsoMe) {
+//   alert ('key: ' + key + '\n' + 'value: ' + alsoMe[key]);
+//  }
 
 
 //NEXT PROBLEM
@@ -82,11 +108,21 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code Here
 
+  var album = {
+    funky: "3:15",
+    ballad: "2:14",
+    pops: "5:12",
+    height: "4:22",
+    dropItLikeItsHot: "3:33"
+  }
+
 //Now, loop through your album object alerting every song title individually.
 
   //Code Here
 
-
+// for (var key in album) {
+//   alert (key);
+// }
 
 
 //NEXT PROBLEM
@@ -98,11 +134,23 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code Here
 
+  var states = {
+    utah: "3000000",
+    colorado:  "6000000",
+    california: "50000000",
+    wyoming: "1000000",
+    idaho: "20000"
+  }
+
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
   //Code Here
 
-
+// for (var key in states) {
+//   if (states[key] > 30000) {
+//     alert(key);
+//   }
+// }
 
 
 //NEXT PROBLEM
@@ -123,11 +171,21 @@ that each value is truthy. If it's not truthy, remove it from the object. */
 
   //Code Here
 
+  for (var key in user1) {
+    if (!user1[key]) {
+      delete user1[key];
+    }
+  }
+
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
   //Code Here
 
-
+user1 = {
+  name: "Jordan",
+  username: "jumprope",
+  pwHash: "44ksowiehls"
+}
 
 
 //NEXT PROBLEM
@@ -151,11 +209,14 @@ var user2 = {
 
   //Code Here
 
+  user2.name = "Tyler S. McGinnis";
+  user2.email = "tyler.mcginnis@devmounta.in";
+
 //Now call the sayName method that's on the user object which will alert the users email
 
   //Code Here
 
-
+// user2.sayName()
 
 
 //NEXT PROBLEM
@@ -167,16 +228,23 @@ var user2 = {
 
   //Code Here
 
+  var methodCollection = {}
+
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
+
   //Code Here
+
+  methodCollection.alertHello = function() {alert("hello")};
+  methodCollection.logHello = function() {console.log("hello")};
 
 //Now call your alertHello and logHello methods.
 
   //Code Here
 
+// methodCollection.alertHello()
 
 
 //NEXT PROBLEM
@@ -187,6 +255,15 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
   //Code Here
 
+  function MakePerson(one, two, three) {
+    var makePerson = {
+      name: one,
+      birthday: two,
+      ssn: three
+    }
+    return makePerson;
+  }
+
 
 
 //NEXT PROBLEM
@@ -196,12 +273,24 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 // Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
 
   //Code Here
-  
-  
-  
+
+  function MakeCard (one, two, three) {
+    var newCard = {
+      name: one,
+      number: two,
+      ccv: three
+    }
+    return newCard;
+  }
+
+
+
 //NEXT PROBLEM
 
+//person = {name: J, age: 50}
+//cc = {number: 12345678}
 
+//result = {name, age, number}
 
 /* As of this point you should have a MakePerson and a MakeCard function which returns you either a person or a credit card object.
    Now, create a bindCard function that takes in a person object as its first parameter and a creditcard object as its second parameter.
@@ -210,4 +299,17 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
   //Code Here
 
-
+function bindCard(person, creditcard) {
+  var merged = {}
+  for (var key in person) {
+    if (person.hasOwnProperty(key)) {
+      merged[key] = person[key];
+    }
+  }
+  for (var key in creditcard) {
+    if (person.hasOwnProperty(key)) {
+      merged[key] = creditcard[key];
+      }
+  }
+  return merged;
+}
